6ae3d36c25f099380366ccb74eb180fc7559e8d1
insan <usaiyrovinsan@gmail.com>
2023-08-26 12:47:30 +0600

    did the layout of the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import './App.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [data, setData] = useState([])
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	useEffect(() => {
+		axios.get(import.meta.env.VITE_BASE_API_URL).then(res => setData(res.data))
+	}, [])
+
+	const graph = () => {
+		const days = []
+		for (let i = 1; i <= 357; i++) {
+			days.push(i)
+		}
+		return days
+	}
+	return (
+		<div className='app'>
+			<div className='container'>
+				<div className='graph'>
+					<ul className='graph__months'>
+						<li className='graph__months-item'>я</li>
+						<li className='graph__months-item'>ф</li>
+						<li className='graph__months-item'>м</li>
+						<li className='graph__months-item'>а</li>
+						<li className='graph__months-item'>м</li>
+						<li className='graph__months-item'>и</li>
+						<li className='graph__months-item'>и</li>
+						<li className='graph__months-item'>а</li>
+						<li className='graph__months-item'>с</li>
+						<li className='graph__months-item'>о</li>
+						<li className='graph__months-item'>н</li>
+						<li className='graph__months-item'>д</li>
+					</ul>
+					<ul className='graph__week'>
+						<li className='graph__week-item'>пн</li>
+						<li className='graph__week-item'>ср</li>
+						<li className='graph__week-item'>пт</li>
+					</ul>
+					<ul className='graph__squares'>
+						{graph().map(day => (
+							<li className='square' key={day}></li>
+						))}
+					</ul>
+				</div>
+				<div className='example'>
+					<div className='example__txt'>
+						<p>Меньше</p>
+					</div>
+					<ul className='example__squares'>
+						<li className='square'></li>
+						<li className='square lvl1'></li>
+						<li className='square lvl2'></li>
+						<li className='square lvl3'></li>
+						<li className='square lvl4'></li>
+					</ul>
+					<div className='example__txt'>
+						<p>Больше</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default App
